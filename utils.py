e017e4e7279d3de162169cf913df4c38f5a6ee75
大神卓也 <ogami@dyn71.eidos.ic.i.u-tokyo.ac.jp>
2022-04-30 17:08:49 +0900

    modified

---
+++ utils.py
@@ -0,0 +1,8 @@
+import datetime
+
+def cur_time():
+    t_delta = datetime.timedelta(hours=9)
+    JST = datetime.timezone(t_delta, 'JST')
+    now = datetime.datetime.now(JST)
+    d = now.strftime('%Y%m%d%H%M%S')
+    return d[2:]
